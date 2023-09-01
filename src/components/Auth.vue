942a2f20bc6355e5ba40abdb46e881399cb4e8a7
OM PRAKASH KANNAN <omprakash.kannan@rootquotient.com>
2023-09-01 20:18:08 +0530

    Implemented login module

---
+++ src/components/Auth.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="auth">
+    <div class="auth__container">
+      <div :class="container">
+        <p :class="container + '__greet'">Welcome back to <span>Aran Pani</span></p>
+        <LoginForm/>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import LoginForm from './LoginForm.vue'
+export default {
+  name: `Auth`,
+  components:{
+    LoginForm
+  },
+  data(){
+    return{
+      container: 'auth__container__login-component',
+    }
+  }
+}
+</script>
+
+<style>
+@import './Auth.scss';
+</style>
