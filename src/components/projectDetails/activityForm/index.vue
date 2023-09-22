c0a2e3192116966ae2f97082d8350985afe56c6c
OM PRAKASH KANNAN <omprakash.kannan@rootquotient.com>
2023-09-22 17:43:00 +0530

    document upload and gmap is implemented

--- src/components/projectDetails/activityForm/index.vue
+++ src/components/projectDetails/activityForm/index.vue
@@ -1,65 +1,68 @@
 <template>
-    <form v-show="newActivity" class="project-details-component__main-container__info__activity__container" @submit.prevent="addActivity">
-        <p class="project-details-component__main-container__info__activity__container__date"><span class="project-details-component__main-container__info__activity__container__date-dot"></span>{{ new Date().toLocaleDateString() }}</p>
-        <div class="project-details-component__main-container__info__activity__container__content">
-            <div class="project-details-component__main-container__info__activity__container__content__img-btn">
-                <div class="activity-preview-screen">
-                    <span v-for="image in projectBlogAttachments" :key="image.id">
-                        <embed v-if="image.image_url != null" :src="image.image_url" :id="image.id" class="activity-img"/>
-                    </span>
-                    <input type="file" id="new-activity-image" @change.prevent="blogUpload" style="display: none;">
-                    <label for="new-activity-image">
-                        <img src="@/assets/upload-activity-img.png" alt="Upload Image" class="activity-img">
-                    </label>
+    <div v-show="projectData.status !== 'proposed' && projectData.status !== 'planned'" class="project-details-component__main-container__info__activity">
+        <p class="project-details-component__main-container__info__activity__title">Activity<span><img src="@/assets/plus-icon.png" id="addActivity" class="project-details-component__main-container__info__activity__title__add-icon" @click.prevent="newActivity = !newActivity"/></span></p>       
+        <form v-show="newActivity" class="project-details-component__main-container__info__activity__container" @submit.prevent="addActivity">
+            <p class="project-details-component__main-container__info__activity__container__date"><span class="project-details-component__main-container__info__activity__container__date-dot"></span>{{ new Date().toLocaleDateString() }}</p>
+            <div class="project-details-component__main-container__info__activity__container__content">
+                <div class="project-details-component__main-container__info__activity__container__content__img-btn">
+                    <div class="activity-preview-screen">
+                        <span v-for="image in projectBlogAttachments" :key="image.id">
+                            <embed v-if="image.image_url != null" :src="image.image_url" :id="image.id" class="activity-img"/>
+                        </span>
+                        <input type="file" id="new-activity-image" @change.prevent="blogUpload" style="display: none;">
+                        <label for="new-activity-image">
+                            <img src="@/assets/upload-activity-img.png" alt="Upload Image" class="activity-img">
+                        </label>
+                    </div>
+                    <p class="activity-control-btn">
+                        <button type="submit" class="activity-done-btn">Update</button>
+                        <button @click.prevent="cancelActivityForm" class="activity-cancel-btn">Cancel</button>
+                    </p>
                 </div>
-                <p class="activity-control-btn">
-                    <button type="submit" class="activity-done-btn">Update</button>
-                    <button @click.prevent="cancelActivityForm" class="activity-cancel-btn">Cancel</button>
-                </p>
-            </div>
-            <div class="activity-description">
-                <label for="description-eng" class="input-label">Description (English)</label>
-                <textarea id="new-description-eng" placeholder="Type here ... " v-model="descriptionEng" class="activity-description-area"></textarea>
-                <p for="new-description-eng" class="error-input" v-show="descriptionEng == ''"> Description is required </p>
-            </div>
-            <div class="activity-description">
-                <label for="description-tam" class="input-label">விளக்கம் (தமிழ்)</label>
-                <textarea id="new-description-tam" placeholder="இங்கே தட்டச்சு செய்க ... " v-model="descriptionTam" class="activity-description-area"></textarea>
-                <p for="new-description-tam" class="error-input" v-show="descriptionTam == ''"> Description is required </p>
-            </div>
-        </div>
-    </form>
-    <form v-for="activity in projectData.project_blogs" :key="activity.id" class="project-details-component__main-container__info__activity__container" @submit.prevent="updateActivity(activity)" >
-        <p class="project-details-component__main-container__info__activity__container__date"><span class="project-details-component__main-container__info__activity__container__date-dot"></span>{{ formatUpdateDate(activity.updated_at) }}</p>
-        <div :id="activity.id" class="project-details-component__main-container__info__activity__container__content">
-            <div class="project-details-component__main-container__info__activity__container__content__img-btn">
-                <div class="activity-preview-screen">
-                    <span v-for="image in activity.project_blog_attachments" :key="image.id">
-                        <embed v-if="image.image_url != null" :src="image.image_url" :id="image.id" class="activity-img">
-                    </span>
-                    <input type="file" id="activity-image" @change.prevent="updateBlog($event, activity)" style="display: none;">
-                    <label v-show="activity.isEditActivity" for="activity-image">
-                        <img src="@/assets/upload-activity-img.png" alt="Upload Image" class="activity-img">
-                    </label>
+                <div class="activity-description">
+                    <label for="description-eng" class="input-label">Description (English)</label>
+                    <textarea id="new-description-eng" placeholder="Type here ... " v-model="descriptionEng" class="activity-description-area"></textarea>
+                    <p for="new-description-eng" class="error-input" v-show="descriptionEng == ''"> Description is required </p>
                 </div>
-                <p class="activity-control-btn">
-                    <img v-show="activity.isEditActivity === true ? false : true" @click.prevent="editActivity(activity)" class="activity-edit-btn" src="@/assets/edit-icon.png"/>
-                    <button v-show="activity.isEditActivity" type="submit" class="activity-done-btn">Update</button>
-                    <button v-show="activity.isEditActivity" @click.prevent="cancelEdit(activity)" class="activity-cancel-btn">Cancel</button>
-                </p>
+                <div class="activity-description">
+                    <label for="description-tam" class="input-label">விளக்கம் (தமிழ்)</label>
+                    <textarea id="new-description-tam" placeholder="இங்கே தட்டச்சு செய்க ... " v-model="descriptionTam" class="activity-description-area"></textarea>
+                    <p for="new-description-tam" class="error-input" v-show="descriptionTam == ''"> Description is required </p>
+                </div>
             </div>
-            <div class="activity-description">
-                <label for="description-eng" class="input-label">Description (English)</label>
-                <textarea :disabled="!activity.isEditActivity" id="description-eng" placeholder="Type here ... " v-model="activity.content_english" class="activity-description-area" :class="{ 'activity-view-mode': !activity.isEditActivity }"></textarea>
-                <p for="description-eng" class="error-input" v-show="activity.content_english == '' && activity.isEditActivity"> Description is required </p>
+        </form>
+        <form v-for="activity in projectData.project_blogs" :key="activity.id" class="project-details-component__main-container__info__activity__container" @submit.prevent="updateActivity(activity)" >
+            <p class="project-details-component__main-container__info__activity__container__date"><span class="project-details-component__main-container__info__activity__container__date-dot"></span>{{ formatUpdateDate(activity.updated_at) }}</p>
+            <div :id="activity.id" class="project-details-component__main-container__info__activity__container__content">
+                <div class="project-details-component__main-container__info__activity__container__content__img-btn">
+                    <div class="activity-preview-screen">
+                        <span v-for="image in activity.project_blog_attachments" :key="image.id">
+                            <embed v-if="image.image_url != null" :src="image.image_url" :id="image.id" class="activity-img">
+                        </span>
+                        <input type="file" id="activity-image" @change.prevent="updateBlog($event, activity)" style="display: none;">
+                        <label v-show="activity.isEditActivity" for="activity-image">
+                            <img src="@/assets/upload-activity-img.png" alt="Upload Image" class="activity-img">
+                        </label>
+                    </div>
+                    <p class="activity-control-btn">
+                        <img v-show="activity.isEditActivity === true ? false : true" @click.prevent="editActivity(activity)" class="activity-edit-btn" src="@/assets/edit-icon.png"/>
+                        <button v-show="activity.isEditActivity" type="submit" class="activity-done-btn">Update</button>
+                        <button v-show="activity.isEditActivity" @click.prevent="cancelEdit(activity)" class="activity-cancel-btn">Cancel</button>
+                    </p>
+                </div>
+                <div class="activity-description">
+                    <label for="description-eng" class="input-label">Description (English)</label>
+                    <textarea :disabled="!activity.isEditActivity" id="description-eng" placeholder="Type here ... " v-model="activity.content_english" class="activity-description-area" :class="{ 'activity-view-mode': !activity.isEditActivity }"></textarea>
+                    <p for="description-eng" class="error-input" v-show="activity.content_english == '' && activity.isEditActivity"> Description is required </p>
+                </div>
+                <div class="activity-description">
+                    <label for="description-tam" class="input-label">விளக்கம் (தமிழ்)</label>
+                    <textarea :disabled="!activity.isEditActivity" id="description-tam" placeholder="இங்கே தட்டச்சு செய்க ... " v-model="activity.content_tamil" class="activity-description-area" :class="{ 'activity-view-mode': !activity.isEditActivity }"></textarea>
+                    <p for="description-tam" class="error-input" v-show="activity.content_english == '' && activity.isEditActivity"> Description is required </p>
+                </div>
             </div>
-            <div class="activity-description">
-                <label for="description-tam" class="input-label">விளக்கம் (தமிழ்)</label>
-                <textarea :disabled="!activity.isEditActivity" id="description-tam" placeholder="இங்கே தட்டச்சு செய்க ... " v-model="activity.content_tamil" class="activity-description-area" :class="{ 'activity-view-mode': !activity.isEditActivity }"></textarea>
-                <p for="description-tam" class="error-input" v-show="activity.content_english == '' && activity.isEditActivity"> Description is required </p>
-            </div>
-        </div>
-    </form>
+        </form>
+    </div>
 </template>
 
 <script>
@@ -72,17 +75,15 @@ export default{
             type: Object,
             required: true, 
         },
-        newActivity: Boolean,
-    },
-    emits:{
-        newActivity: Boolean,
     },
     setup(){
+        const newActivity = ref(false);
         const descriptionEng = ref('');
         const descriptionTam = ref('');
         const projectBlogAttachments = ref([]);
         const projectBlogAttachmentIds = ref([]);
         return{
+            newActivity,
             descriptionEng,
             descriptionTam,
             projectBlogAttachments,
@@ -151,7 +152,7 @@ export default{
             this.projectBlogAttachmentIds = [];
             this.descriptionEng = [];
             this.descriptionTam = [];
-            this.$emit('newActivity',false);
+            this.newActivity = false;
         },
         editActivity(activity) {
             activity.isEditActivity = true;
--- src/components/projectDetails/documentForm/index.vue
+++ src/components/projectDetails/documentForm/index.vue
@@ -1,10 +1,23 @@
 <template>
-    <div>
-        
+    <div class="doc-form">
+        <span v-if="projectData !== null ? previewDoc = projectData.project_documents : previewDoc = []"></span>
+        <p class="doc-form__title">Documents</p>
+        <p class="doc-form__description">Add documents related to project</p>
+        <div v-for="doc in previewDoc" :key="doc.id" class="doc-form__preview">
+            <embed v-if="doc.document_url != null" :id="doc.id" :src="doc.document_url" class="doc-form__doc"/>
+        </div>
+        <div class="doc-form__preview">
+            <label for="document-upload">
+                <img src="@/assets/upload-doc-form.png" class="doc-form__doc">
+            </label>
+            <input type="file" accept="application/pdf" @change="attachDocument" id="document-upload" style="display: none;">
+        </div>
     </div>
 </template>
 
 <script>
+import { ref } from 'vue';
+import ProjectService from '@/services/ProjectService';
 export default{
     name: 'DocumentForm',
     props:{
@@ -14,10 +27,22 @@ export default{
         }
     },
     setup(){
+        const previewDoc = ref([]);
         return{
-
+            previewDoc,
         }
     },
+    methods:{
+        async attachDocument(event){
+            if(this.projectData){
+                const selectedImage = event.target.files[0];
+                const response = await ProjectService.uploadDocument(this.projectData.id, selectedImage);
+                if(response && response.status == 200){
+                    this.previewDoc.push(response.data.project_document);
+                }
+            }
+        }
+    }
 }
 </script>
 
